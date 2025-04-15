4aaab13bbecb00672df7f747d1b362d930a05310
Liam Appleton <liamappleton97@outlook.com>
2025-04-15 09:36:40 +0100

    initial commit

---
+++ src/test/java/org/spiceboys/Travel/Diary/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spiceboys.Travel.Diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
