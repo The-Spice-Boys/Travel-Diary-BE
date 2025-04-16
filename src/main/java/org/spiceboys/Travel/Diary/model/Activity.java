15ede22087d4832ab13a46d7aef8d32bdd7811d3
Shemz-ux <shemaiahnguessan@outlook.com>
2025-04-16 12:37:16 +0100

    Finalised activity table

--- src/main/java/org/spiceboys/Travel/Diary/model/Activity.java
+++ src/main/java/org/spiceboys/Travel/Diary/model/Activity.java
@@ -2,16 +2,19 @@ package org.spiceboys.Travel.Diary.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
+
 @Entity
-@Table(name="activity")
+@Table(name="activities")
 public class Activity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer activityId;
 
 
     @ManyToOne
@@ -26,18 +29,35 @@ public class Activity {
 
     private Boolean completeStatus;
 
+    @OneToMany(
+            mappedBy = "activities"
+    )
+    @JsonManagedReference
     private List<Note> notes;
 
+    @OneToMany(
+            mappedBy = "activities"
+    )
+    @JsonManagedReference
     private List<Photo> photos;
 
-    public Activity(String title, Boolean completeStatus) {
+    public Activity() {}
+
+    public Activity(String title, Boolean completeStatus, Itinerary itinerary) {
         this.title = title;
         this.completeStatus = completeStatus;
-        this.notes = notes;
-        this.photos = photos; //is this correct?
+        this.itinerary = itinerary;
+        this.notes = new ArrayList<>();
+        this.photos = new ArrayList<>();
     }
 
-    public Activity() {}
+    public Itinerary getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(Itinerary itinerary) {
+        this.itinerary = itinerary;
+    }
 
     public Integer getId() {
         return id;
