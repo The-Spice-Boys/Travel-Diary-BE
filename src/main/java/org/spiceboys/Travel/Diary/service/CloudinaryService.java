9e9659e92335c042412f25e0b90652e77e048948
MWM02 <wijem004@gmail.com>
2025-04-17 16:49:54 +0100

    adds cloudinary config

---
+++ src/main/java/org/spiceboys/Travel/Diary/service/CloudinaryService.java
@@ -0,0 +1,21 @@
+package org.spiceboys.Travel.Diary.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap()).get("secure_url").toString();
+    }
+}
