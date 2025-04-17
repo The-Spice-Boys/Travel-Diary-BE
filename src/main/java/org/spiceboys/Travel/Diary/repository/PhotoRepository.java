5219bb13fee952c04c2ae38b3c06d36453ae91db
Liam Appleton <liamappleton97@outlook.com>
2025-04-17 15:09:29 +0100

    creating PhotoRepository

--- src/main/java/org/spiceboys/Travel/Diary/repository/PhotoRepository.java
+++ src/main/java/org/spiceboys/Travel/Diary/repository/PhotoRepository.java
@@ -1,4 +1,6 @@
 package org.spiceboys.Travel.Diary.repository;
 
-public interface PhotoRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.spiceboys.Travel.Diary.model.Photo;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {}
