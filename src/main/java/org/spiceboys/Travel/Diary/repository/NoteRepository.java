4670df81ac23bf90c71e16e3db9b516f46585092
MWM02 <wijem004@gmail.com>
2025-04-17 12:04:49 +0100

    creates notes seeder

--- src/main/java/org/spiceboys/Travel/Diary/repository/NoteRepository.java
+++ src/main/java/org/spiceboys/Travel/Diary/repository/NoteRepository.java
@@ -1,4 +1,10 @@
 package org.spiceboys.Travel.Diary.repository;
 
-public interface NoteRepository {
+import org.spiceboys.Travel.Diary.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
 }
--- src/main/java/org/spiceboys/Travel/Diary/seeder/NoteSeeder.java
+++ src/main/java/org/spiceboys/Travel/Diary/seeder/NoteSeeder.java
@@ -1,4 +1,56 @@
 package org.spiceboys.Travel.Diary.seeder;
 
-public class NoteSeeder {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.spiceboys.Travel.Diary.exception.ContentNotFoundException;
+import org.spiceboys.Travel.Diary.model.Activity;
+import org.spiceboys.Travel.Diary.model.Note;
+import org.spiceboys.Travel.Diary.repository.ActivityRepository;
+import org.spiceboys.Travel.Diary.repository.NoteRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.io.InputStream;
+import java.util.Optional;
+
+@Component
+@Order(4)
+class NoteSeeder implements CommandLineRunner {
+    private final NoteRepository noteRepository;
+
+    private final ActivityRepository activityRepository;
+
+    private final ObjectMapper objectMapper;
+
+    public NoteSeeder(NoteRepository noteRepository, ActivityRepository activityRepository,ObjectMapper objectMapper) {
+        this.noteRepository = noteRepository;
+        this.activityRepository = activityRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (noteRepository.count() == 0) {
+            InputStream resourceAsStream = this.getClass().getResourceAsStream("/data/notes.json");
+            if (resourceAsStream != null) {
+                List<Note> notes = objectMapper.readValue(resourceAsStream, new TypeReference<List<Note>>(){});
+                for (Note note : notes) {
+                    Long activityId = note.getActivity().getActivityId();
+                    Optional<Activity> activity = activityRepository.findById(activityId);
+                    if (activity.isPresent()) {
+                        note.setActivity(activity.get());
+                    } else {
+                        throw new ContentNotFoundException("Notes not found");
+                    }
+                }
+                noteRepository.saveAll(notes);
+                System.out.println("✅ Database seeded with " + notes.size() + " notes");
+            } else {
+                System.out.println("ℹ Could not find notes.json");
+            }
+        } else {
+            System.out.println("ℹ Database already seeded with notes");
+        }
+    }
 }
