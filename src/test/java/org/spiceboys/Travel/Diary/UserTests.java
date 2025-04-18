80407d12c1072b18e36263287b16a9eefdf57666
jabc8 <jt.024@outlook.com>
2025-04-18 19:21:21 +0100

    Add notes to UserTests, declare methods for testing PATCH requests to update user information

--- src/test/java/org/spiceboys/Travel/Diary/UserTests.java
+++ src/test/java/org/spiceboys/Travel/Diary/UserTests.java
@@ -12,6 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserTests {
+
+    /*
+    Current DTO logic will return a private DTO even for the logged-in user, meaning the logged-in user won't be able
+    to see all of their data. Will need to validate/confirm logged-in user on the back-end?? and write tests to account
+    for this. - JT
+    */
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -39,13 +46,29 @@ public class UserTests {
                                 "\"profilePicUrl\":  \"https://picsum.photos/seed/picsum/200/300\", " +
                                 "\"private\": false}"));
 
-        // If private user, returns a JSON with only the 'private' property
+        /*
+        If private user, returns a JSON with the following properties:
+            -- userId
+            -- username
+            -- private
+        */
         this.mockMvc
                 .perform(get("/api/users/username/nomad_mike"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(
                         "{\"private\": true}"));
+
+        /*
+        If logged-in user is requesting their own data, ALWAYS returns a JSON with the following properties:
+            -- userId
+            -- username
+            -- bio
+            -- profilePicUrl
+            -- private
+        */
+        // TEST GOES HERE!!!
+
 
         // Returns 404 Not Found status when username does not exist in database
         this.mockMvc
@@ -75,7 +98,12 @@ public class UserTests {
                                 "\"profilePicUrl\":  \"https://picsum.photos/seed/picsum/200/300\", " +
                                 "\"private\": false}"));
 
-        // If private user, returns a JSON with only the 'private' property
+        /*
+        If private user, returns a JSON with the following properties:
+            -- userId
+            -- username
+            -- private
+        */
         this.mockMvc
                 .perform(get("/api/users/userId/3"))
                 .andDo(print())
@@ -83,10 +111,63 @@ public class UserTests {
                 .andExpect(content().json(
                         "{\"private\": true}"));
 
+        /*
+        If logged-in user is requesting their own data, ALWAYS returns a JSON with the following properties:
+            -- userId
+            -- username
+            -- bio
+            -- profilePicUrl
+            -- private
+        */
+        // TEST GOES HERE!!!
+
         // Returns 404 Not Found status when user ID does not exist in database
         this.mockMvc
                 .perform(get("/api/users/username/exploratory_dave"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
     }
+
+    @Test
+    public void createUser() throws Exception {
+        /*
+        Adds a new user to the database and returns a JSON object with the following properties:
+            -- userId
+            -- username
+            -- bio
+            -- profilePicUrl
+            -- private
+        */
+        this.mockMvc
+                .perform(post("/api/users")
+                        .contentType("application/json")
+                        .content("{\"username\": \"exploratory_dave\", " +
+                                "\"firstName\": \"Dave\", " +
+                                "\"lastName\": \"Davidson\", " +
+                                "\"email\": \"dave@example.com\", " +
+                                "\"password\": \"davepassword\", " +
+                                "\"bio\": \"Hi I'm Dave and I like to explore!\", " +
+                                "\"profilePicUrl\": \"https://example.com\", " +
+                                "\"isPrivate\": false}")
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().json(
+                        "{\"userId\": 5, " +
+                        "\"username\": \"exploratory_dave\", " +
+                        "\"bio\": \"Hi I'm Dave and I like to explore!\", " +
+                        "\"profilePicUrl\": \"https://example.com\", " +
+                        "\"private\": false}"));
+
+        // ERROR TESTS
+    }
+
+    @Test
+    public void updatePublicUserInfo() throws Exception {}
+
+    @Test
+    public void updatePrivateUserInfo() throws Exception {}
+
+    @Test
+    public void updateUserPassword() throws Exception {}
 }
