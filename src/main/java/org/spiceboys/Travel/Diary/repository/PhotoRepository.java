ef804394e47202c8e80ad0e755b4df946de4de19
Shemz-ux <shemaiahnguessan@outlook.com>
2025-04-22 15:11:08 +0100

    Photo endpoints complete

--- src/main/java/org/spiceboys/Travel/Diary/repository/PhotoRepository.java
+++ src/main/java/org/spiceboys/Travel/Diary/repository/PhotoRepository.java
@@ -1,6 +1,11 @@
 package org.spiceboys.Travel.Diary.repository;
 
+import org.spiceboys.Travel.Diary.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.spiceboys.Travel.Diary.model.Photo;
 
-public interface PhotoRepository extends JpaRepository<Photo, Long> {}
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    List<Photo> findByActivity(Activity activity);
+}
